e43d4b9e015d9fc580d56109e4c61a35f634d580
Steven Tang <steven@steventang.net>
2022-01-26 17:20:31 +1100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{env,process};
+
+fn main() {
+    let mut pretty = false;
+    let mut filename = None;
+
+    let arguments: Vec<String> = env::args().collect();
+    for (_, argument) in arguments.iter().enumerate() {
+        if argument == "--help" {
+            println!("usage: {} [-p] input", arguments[0]);
+            process::exit(1);
+        }
+        if argument == "-p" {
+            pretty = true;
+            continue;
+        }
+        filename = Some(argument);
+    }
+
+    let filename = match filename {
+        None => {
+            eprintln!("Filename not given");
+            process::exit(1);
+        },
+        Some(value) => value,
+    };
+
+    println!("{} invoked with file: {}, pretty: {}", arguments[0], filename, pretty);
+}
