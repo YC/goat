d6d1e8bd84772339ff1a9a237cc1ddc2a60c11e0
Steven Tang <yc@steventang.net>
2022-10-31 21:12:19 +1100

    fix: arity, empty statement, matrix > 0

--- tests/test.sh
+++ tests/test.sh
@@ -8,7 +8,7 @@ if [ ! -f "$goat" ]; then
     exit 1
 fi
 
-for f in "$(dirname "$0")"/stage1-visible/* "$(dirname "$0")"/fortytwo/*; do
+for f in "$(dirname "$0")"/stage1-visible/* "$(dirname "$0")"/fortytwo/* "$(dirname "$0")"/new/*; do
     eval "$goat" -p "$f" &> /dev/null
 
     rv=$?
